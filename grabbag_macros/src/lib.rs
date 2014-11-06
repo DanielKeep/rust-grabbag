aa81c0434c6ad4d2ae47a27ef259ec4319b77aaa
Daniel Keep <daniel.keep@gmail.com>
2014-11-06 16:11:03 +1100

    Added sequence! and recurrence! macros.

--- grabbag_macros/src/lib.rs
+++ grabbag_macros/src/lib.rs
@@ -82,3 +82,163 @@ macro_rules! count_exprs {
     () => { 0 };
     ($e:expr $(, $es:expr)*) => { 1 + count_exprs!($($es),*) };
 }
+
+/**
+Expands to an expression implementing the `Iterator` trait, which yields successive
+elements of the given closed-form sequence.
+
+For example, you can define the sequence of positive odd integers like so:
+
+```
+# #![feature(phase)]
+# #[phase(plugin)] extern crate grabbag_macros;
+# fn main() {
+#     let _ =
+sequence![ n: u64 = 2*(n as u64) + 1 ]
+#     ;
+# }
+```
+
+You can also specify one or more initial members of the sequence that are also used in the closed form expression like so:
+
+```
+# #![feature(phase)]
+# #[phase(plugin)] extern crate grabbag_macros;
+# fn main() {
+#     let _ =
+sequence![ a[n]: u64 = 1, 2... a[0]*(n as u64) + a[1] ]
+#     ;
+# }
+```
+*/
+#[macro_export]
+macro_rules! sequence {
+    ( $ind:ident: $sty:ty = $closed_form:expr ) => {
+        {
+            struct Sequence {
+                pos: uint,
+            }
+
+            impl Iterator<$sty> for Sequence {
+                #[inline]
+                fn next(&mut self) -> Option<$sty> {
+                    let next_val: $sty = {
+                        let $ind = self.pos;
+                        $closed_form
+                    };
+
+                    self.pos += 1;
+                    Some(next_val)
+                }
+            }
+
+            Sequence { pos: 0 }
+        }
+    };
+    ( $seq:ident [ $ind:ident ]: $sty:ty = $($inits:expr),+ ... $closed_form:expr ) => {
+        {
+            const INITS: uint = count_exprs!($($inits),+);
+
+            struct Sequence {
+                inits: [$sty, ..INITS],
+                pos: uint,
+            }
+
+            impl Iterator<$sty> for Sequence {
+                #[inline]
+                fn next(&mut self) -> Option<$sty> {
+                    if self.pos < INITS {
+                        let next_val = self.inits[self.pos];
+                        self.pos += 1;
+                        Some(next_val)
+                    } else {
+                        let next_val: $sty = {
+                            let $ind = self.pos;
+                            let $seq = &self.inits;
+                            $closed_form
+                        };
+
+                        self.pos += 1;
+                        Some(next_val)
+                    }
+                }
+            }
+
+            Sequence { inits: [$($inits),+], pos: 0 }
+        }
+    };
+}
+
+/**
+Expands to an expression implementing the `Iterator` trait, which yields successive
+elements of the given recurrence relationship.
+
+For example, you can define a Fibonnaci sequence iterator like so:
+
+```
+# #![feature(phase)]
+# #[phase(plugin)] extern crate grabbag_macros;
+# fn main() {
+#     let _ =
+recurrence![ fib[n]: f64 = 0.0, 1.0 ... fib[n-1] + fib[n-2] ]
+#     ;
+# }
+```
+*/
+#[macro_export]
+macro_rules! recurrence {
+    ( $seq:ident [ $ind:ident ]: $sty:ty = $($inits:expr),+ ... $recur:expr ) => {
+        {
+            const MEMORY: uint = count_exprs!($($inits),+);
+
+            struct Recurrence {
+                mem: [$sty, ..MEMORY],
+                pos: uint,
+            }
+
+            struct IndexOffset<'a> {
+                slice: &'a [$sty, ..MEMORY],
+                offset: uint,
+            }
+
+            impl<'a> Index<uint, $sty> for IndexOffset<'a> {
+                #[inline(always)]
+                fn index<'b>(&'b self, index: &uint) -> &'b $sty {
+                    let real_index = *index - self.offset + MEMORY;
+                    &self.slice[real_index]
+                }
+            }
+
+            impl Iterator<$sty> for Recurrence {
+                #[inline]
+                fn next(&mut self) -> Option<$sty> {
+                    if self.pos < MEMORY {
+                        let next_val = self.mem[self.pos];
+                        self.pos += 1;
+                        Some(next_val)
+                    } else {
+                        let next_val: $sty = {
+                            let $ind = self.pos;
+                            let $seq = IndexOffset { slice: &self.mem, offset: $ind };
+                            $recur
+                        };
+
+                        {
+                            use std::mem::swap;
+
+                            let mut swap_tmp = next_val;
+                            for i in range(0, MEMORY).rev() {
+                                swap(&mut swap_tmp, &mut self.mem[i]);
+                            }
+                        }
+
+                        self.pos += 1;
+                        Some(next_val)
+                    }
+                }
+            }
+
+            Recurrence { mem: [$($inits),+], pos: 0 }
+        }
+    };
+}
