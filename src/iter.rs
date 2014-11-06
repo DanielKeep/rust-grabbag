623a30161828f8ae4f5e441267ab8ce267ef6960
Daniel Keep <daniel.keep@gmail.com>
2014-11-06 18:49:45 +1100

    Added round_robin and round_robin_longest.

--- src/iter.rs
+++ src/iter.rs
@@ -1,3 +1,5 @@
+use std::cmp::min;
+
 pub trait IteratorCloneEach<'a, E, It> where E: Clone, It: Iterator<&'a E> {
     /**
 Creates an iterator which will clone each element of the input iterator.
@@ -99,6 +101,151 @@ fn test_foldr() {
     let vs = vec!["a", "b", "c"];
     let vs = vs.into_iter().map(|e| e.into_string());
     assert_eq!(Some("(a, (b, c))".into_string()), vs.foldr(|a,b| format!("({}, {})", a, b)));
+}
+
+pub trait IteratorRoundRobin<E, It1> where It1: Iterator<E> {
+    /**
+Creates an iterator that alternates between yielding elements of the two input iterators.  It stops as soon as either iterator is exhausted.
+    */
+    fn round_robin(self, it1: It1) -> RoundRobinItems<E, Self, It1>;
+
+    /**
+Creates an iterator that alternates between yielding elements of the two input iterators.  If one iterator stops before the other, it is simply skipped.
+    */
+    fn round_robin_longest(self, it1: It1) -> RoundRobinLongestItems<E, Self, It1>;
+}
+
+impl<E, It0, It1> IteratorRoundRobin<E, It1> for It0 where It0: Iterator<E>, It1: Iterator<E> {
+    fn round_robin(self, it1: It1) -> RoundRobinItems<E, It0, It1> {
+        RoundRobinItems {
+            it0: self,
+            it1: it1,
+            phase: 0,
+        }
+    }
+
+    fn round_robin_longest(self, it1: It1) -> RoundRobinLongestItems<E, It0, It1> {
+        RoundRobinLongestItems {
+            it0: self,
+            it1: it1,
+            phase: 0,
+            fused: false,
+        }
+    }
+}
+
+pub struct RoundRobinItems<E, It0, It1> where It0: Iterator<E>, It1: Iterator<E> {
+    it0: It0,
+    it1: It1,
+    phase: u8,
+}
+
+impl<E, It0, It1> Iterator<E> for RoundRobinItems<E, It0, It1> where It0: Iterator<E>, It1: Iterator<E> {
+    fn next(&mut self) -> Option<E> {
+        match self.phase {
+            0 => match self.it0.next() {
+                None => None,
+                e @ _ => {
+                    self.phase = 1;
+                    e
+                }
+            },
+            _ => match self.it1.next() {
+                None => None,
+                e @ _ => {
+                    self.phase = 0;
+                    e
+                }
+            },
+        }
+    }
+
+    fn size_hint(&self) -> (uint, Option<uint>) {
+        match (self.it0.size_hint(), self.it1.size_hint()) {
+            ((l0, None), (l1, _)) | ((l0, _), (l1, None)) => (l0+l1, None),
+            ((l0, Some(u0)), (l1, Some(u1))) => (l0+l1, Some(2*min(u0, u1)))
+        }
+    }
+}
+
+pub struct RoundRobinLongestItems<E, It0, It1> where It0: Iterator<E>, It1: Iterator<E> {
+    it0: It0,
+    it1: It1,
+    phase: u8,
+    fused: bool,
+}
+
+impl<E, It0, It1> Iterator<E> for RoundRobinLongestItems<E, It0, It1> where It0: Iterator<E>, It1: Iterator<E> {
+    fn next(&mut self) -> Option<E> {
+        match (self.phase, self.fused) {
+            (0, true) => match self.it0.next() {
+                None => None,
+                e @ _ => e,
+            },
+            (_, true) => match self.it1.next() {
+                None => None,
+                e @ _ => e,
+            },
+            (0, false) => match self.it0.next() {
+                None => {
+                    self.phase = 1;
+                    self.fused = true;
+                    self.next()
+                },
+                e @ _ => {
+                    self.phase = 1;
+                    e
+                },
+            },
+            (_, false) => match self.it1.next() {
+                None => {
+                    self.phase = 0;
+                    self.fused = true;
+                    self.next()
+                },
+                e @ _ => {
+                    self.phase = 0;
+                    e
+                },
+            },
+        }
+    }
+
+    fn size_hint(&self) -> (uint, Option<uint>) {
+        match (self.it0.size_hint(), self.it1.size_hint()) {
+            ((l0, None), (l1, _)) | ((l0, _), (l1, None)) => (l0+l1, None),
+            ((l0, Some(u0)), (l1, Some(u1))) => (l0+l1, Some(u0+u1))
+        }
+    }
+}
+
+#[test]
+fn test_round_robin() {
+    let v0 = vec![0u, 2, 4];
+    let v1 = vec![1u, 3, 5, 7];
+    let mut it = v0.into_iter().round_robin(v1.into_iter());
+    assert_eq!(it.next(), Some(0));
+    assert_eq!(it.next(), Some(1));
+    assert_eq!(it.next(), Some(2));
+    assert_eq!(it.next(), Some(3));
+    assert_eq!(it.next(), Some(4));
+    assert_eq!(it.next(), Some(5));
+    assert_eq!(it.next(), None);
+}
+
+#[test]
+fn test_round_robin_longest() {
+    let v0 = vec![0u, 2, 4];
+    let v1 = vec![1u, 3, 5, 7];
+    let mut it = v0.into_iter().round_robin_longest(v1.into_iter());
+    assert_eq!(it.next(), Some(0));
+    assert_eq!(it.next(), Some(1));
+    assert_eq!(it.next(), Some(2));
+    assert_eq!(it.next(), Some(3));
+    assert_eq!(it.next(), Some(4));
+    assert_eq!(it.next(), Some(5));
+    assert_eq!(it.next(), Some(7));
+    assert_eq!(it.next(), None);
 }
 
 pub trait IteratorStride<E, It> where It: Iterator<E> {
