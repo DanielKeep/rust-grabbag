c9d77fb2b79dbc69363991dc48bfd17fa00920ad
Daniel Keep <daniel.keep@gmail.com>
2014-11-07 19:06:55 +1100

    Added tee.

--- src/iter.rs
+++ src/iter.rs
@@ -1,5 +1,6 @@
 use std::cell::RefCell;
 use std::cmp::{max, min};
+use std::collections::RingBuf;
 use std::mem::replace;
 use std::num::Bounded;
 use std::rc::Rc;
@@ -1026,6 +1027,138 @@ fn test_take_exactly() {
         v.into_iter().take_exactly(3).collect()
     });
     assert!(r.is_err());
+}
+
+pub trait IteratorTee<E> {
+    /**
+Creates a pair of iterators that will yield the same sequence of values.
+
+The element type must implement Clone.
+    */
+    fn tee(self) -> (TeeItems<E, Self>, TeeItems<E, Self>);
+}
+
+impl<E, It> IteratorTee<E> for It where It: Iterator<E> {
+    fn tee(self) -> (TeeItems<E, It>, TeeItems<E, It>) {
+        let tee0 = TeeItems {
+            state: Rc::new(RefCell::new(TeeItemsState {
+                iter: self,
+                iter_next: 0,
+                buffer: RingBuf::new(),
+            })),
+            this_next: 0,
+        };
+        let tee1 = TeeItems {
+            state: tee0.state.clone(),
+            this_next: 0,
+        };
+        (tee0, tee1)
+    }
+}
+
+pub struct TeeItems<E, It> {
+    /*
+        /!\ Important /!\
+
+    In order for this to *not* panic, you need to ensure that nothing is re-entrant whilst it holds a mutable reference to the `RefCell`.  This should still be safe from the user's perspective.
+    */
+    state: Rc<RefCell<TeeItemsState<E, It>>>,
+    this_next: uint,
+}
+
+pub struct TeeItemsState<E, It> {
+    iter: It,
+    iter_next: uint,
+    buffer: RingBuf<E>,
+}
+
+impl<E, It> Iterator<E> for TeeItems<E, It> where It: Iterator<E>, E: Clone {
+    fn next(&mut self) -> Option<E> {
+        let mut state = self.state.deref().borrow_mut();
+        let state = state.deref_mut();
+
+        match (self.this_next, state.iter_next, state.buffer.len()) {
+            // Tee is even with iter.
+            (i, j, _) if i == j => {
+                match state.iter.next() {
+                    None => None,
+                    Some(e) => {
+                        self.this_next += 1;
+                        state.iter_next += 1;
+                        state.buffer.push(e.clone());
+                        Some(e)
+                    }
+                }
+            },
+            // Error: Tee is behind iter, nothing in the buffer.
+            (i, j, 0) if i < j => {
+                panic!("tee fell behind iterator, but buffer is empty");
+            },
+            // Tee is behind iter, elements in buffer.
+            (i, j, _) if i < j => {
+                let e = state.buffer.pop_front().unwrap();
+                self.this_next += 1;
+                Some(e)
+            },
+            // Error: Tee is ahead of iter.
+            _ /*(i, j, _) if i > j*/ => {
+                panic!("tee got ahead of iterator");
+            }
+        }
+    }
+
+    fn size_hint(&self) -> (uint, Option<uint>) {
+        let state = self.state.deref().borrow_mut();
+        let state = state.deref();
+
+        match (self.this_next, state.iter_next, state.buffer.len()) {
+            // Tee is even with iter.
+            (i, j, _) if i == j => {
+                state.iter.size_hint()
+            },
+            // Error: Tee is behind iter, nothing in the buffer.
+            (i, j, 0) if i < j => {
+                panic!("tee fell behind iterator, but buffer is empty");
+            },
+            // Tee is behind iter, elements in buffer.
+            (i, j, n) if i < j => {
+                match state.iter.size_hint() {
+                    (lb, None) => (lb+n, None),
+                    (lb, Some(ub)) => (lb+n, Some(ub+n))
+                }
+            },
+            // Error: Tee is ahead of iter.
+            _ /*(i, j, _) if i > j*/ => {
+                panic!("tee got ahead of iterator");
+            }
+        }
+    }
+}
+
+#[test]
+fn test_tee() {
+    let v = vec![0u, 1, 2, 3];
+    let (mut a, mut b) = v.into_iter().tee();
+    assert_eq!(a.collect::<Vec<_>>(), vec![0, 1, 2, 3]);
+    assert_eq!(b.collect::<Vec<_>>(), vec![0, 1, 2, 3]);
+
+    let v = vec![0u, 1, 2, 3];
+    let (mut a, mut b) = v.into_iter().tee();
+    assert_eq!(b.collect::<Vec<_>>(), vec![0, 1, 2, 3]);
+    assert_eq!(a.collect::<Vec<_>>(), vec![0, 1, 2, 3]);
+
+    let v = vec![0u, 1, 2, 3];
+    let (mut a, mut b) = v.into_iter().tee();
+    assert_eq!(a.next(), Some(0));
+    assert_eq!(a.next(), Some(1));
+    assert_eq!(b.next(), Some(0));
+    assert_eq!(b.next(), Some(1));
+    assert_eq!(b.next(), Some(2));
+    assert_eq!(a.next(), Some(2));
+    assert_eq!(b.next(), Some(3));
+    assert_eq!(a.next(), Some(3));
+    assert_eq!(a.next(), None);
+    assert_eq!(b.next(), None);
 }
 
 pub trait IteratorZipLongest {
